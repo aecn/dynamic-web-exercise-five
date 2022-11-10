e0dcccd50efb4098af8a7fcafc1a1f61db6a8a69
annie <ac8502@nyu.edu>
2022-11-10 10:29:53 -0500

    add post to firebase function

--- routes/createPost.js
+++ routes/createPost.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const firestore = require("firebase/firestore");
+const db = firestore.getFirestore();
 
 const createPostForm = `
     <h1>Create Post</h1>
-    <form action="">
-        <input type="text" name="title" placeholder="Title" />
-        <input type="text" name="text" placeholder="Text" />
-        <input type="text" name="author" placeholder="Author" />
+    <form action="/create/submit">
+    <div style="display: flex; flex-direction: column; max-width: 400px;">
+        <label for="postTitle">Title</label>
+          <input type="text" name="postTitle" placeholder="Title" style="margin-bottom: 20px;"/>
+        <label for="postText">Text</label>
+          <input type="text" name="postText" placeholder="Text" style="margin-bottom: 20px;"/>
+        <label for="author">Author</label>
+          <input type="text" name="author" placeholder="Author" style="margin-bottom: 20px;"/>
         <button type="submit">Submit</button>
+      </div>
     </form>
 `;
 
@@ -18,6 +25,34 @@ router.use((req, res, next) => {
 
   router.get("/", (req, res) => {
     res.send(createPostForm);
+  });
+
+  router.get("/submit", (req, res) => {
+    const queryParams = req.query;
+    const title = queryParams.postTitle;
+    const text = queryParams.postText;
+    const author = queryParams.author;
+    const idFromTitle = title.replace(/\s+/g, "-").toLowerCase();    
+    const setBlogPost = firestore.setDoc(
+      firestore.doc(db, "posts", idFromTitle),
+      {
+        title: title,
+        text: text,
+        author: author,
+      });
+
+    setBlogPost
+      .then((response) => {
+        res.send(`
+          <h1>Submission Successful!</h1>
+          <p><a href="/create">Add Another Post</a><p>
+          <p><a href="/">Return Home</a><p>
+      `);
+      })
+    .catch((error) => {
+      console.warn(error);
+      res.send(`Error Submitting: ${error.toString()}`);
+    });
   });
 
   module.exports = router;
